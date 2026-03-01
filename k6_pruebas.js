2b227ae641f66c9d7b5d5d857ccc4aff97efc292
UrielSC24 <2022371107@uteq.edu.mx>
2026-02-28 23:46:23 -0600

    Agregando script de k6 con Project ID 6857910

---
+++ k6_pruebas.js
@@ -0,0 +1,38 @@
+import http from 'k6/http';
+import { sleep, check } from 'k6';
+
+export const options = {
+    stages: [
+        { duration: '30s', target: 20 }, // Sube a 20 usuarios en 30s
+        { duration: '1m', target: 20 },  // Se mantiene 1 minuto
+        { duration: '20s', target: 0 },  // Baja a 0
+    ],
+    ext: {
+        loadimpact: {
+            projectID: 6857910,
+            name: 'Prueba de Carga Capital Flow'
+        }
+    }
+};
+
+export default function () {
+    const BASE_URL = 'https://urielsc24.github.io/pwa';
+
+    // VISTA 1: Simular la entrada a la PWA (Pantalla Principal / Home)
+    const resHome = http.get(`${BASE_URL}/capital_flow_pwa/index.html`);
+
+    check(resHome, {
+        'Carga Vista Home (index) HTTP 200': (r) => r.status === 200,
+    });
+
+    sleep(1);
+
+    // VISTA 2: Simular la navegación hacia la pantalla de Login 
+    const resLogin = http.get(`${BASE_URL}/capital_flow_pwa/login.html`);
+
+    check(resLogin, {
+        'Carga Vista Login HTTP 200': (r) => r.status === 200 || r.status === 404,
+    });
+
+    sleep(1);
+}
